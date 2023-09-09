9c55b899a530e42487800fe42d7c3c2937f8666e
lixinjack <lixinjack@gmail.com>
2023-09-08 23:02:18 -0700

    upload the code not just build part

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,56 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void BallTransform_Start_m563D622B59DDCEEA517F3220294401EB82D3A7D1 (void);
+extern void BallTransform_Update_m441F920DDE6DAA94714A49576D9661E3C0E9B21F (void);
+extern void BallTransform__ctor_m482BF52575CBD675EB9D6D37085B645EE0573FE7 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	BallTransform_Start_m563D622B59DDCEEA517F3220294401EB82D3A7D1,
+	BallTransform_Update_m441F920DDE6DAA94714A49576D9661E3C0E9B21F,
+	BallTransform__ctor_m482BF52575CBD675EB9D6D37085B645EE0573FE7,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	1098,
+	1098,
+	1098,
+	2121,
+	1098,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
--- README.md
+++
@@ -1,4 +0,0 @@
-# programmatically-alter-object-properties
-Essentials of programming : Unity project where scripts programmatically alter object properties
-
-Playable link: https://play.unity.com/mg/other/hw1q2-zig
